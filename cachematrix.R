e195203eb0da9a6a1e40f1d72cbcb6f1c30f9bea
Kirk Humphreys <mr.capt.kirk@gmail.com>
2014-11-19 22:31:40 -0500

    Corrected misspellings and updated remarks

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,9 @@
-## Put comments here that give an overall description of what your
-##  functions do
-## These funtions take an invertable matrix and cashe the Inverse
+
+## These funtions take an invertable matrix and cashes the Inverse
 ##  matrix 
 
-## Write a short comment describing this function
 
-##This function creates a special "matrix" object that will be cached
+##This function creates a special "matrix" object that will cache
 ##  the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -14,6 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
           x<<-y
           m<<-NULL
      }
+     
      get<-function() x
      setmatrix<-function(solve) m<<- solve
      getmatrix<-function() m
@@ -23,8 +22,6 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
-## Write a short comment describing this function
 ## This function computes the inverse of the special "matrix" 
 ## returned by the makeCacheMatrix and caches the result.
 ## If the reult is already cached then the funtion returns 
@@ -35,12 +32,12 @@ cacheSolve <- function(x, ...) {
      
      m<-x$getmatrix()   # accesses the object 'x' and get the cached inverted matrix
      
-     if(!is.null(m)) {  # if inverted matrix cached (not NULL)     
+     if(!is.null(m)) {  # if inverted matrix cached (not NULL) then     
           message("getting cached matrix")  # ... send this message to the console
           return(m)                         # ... and return the cached inverted matrix 
      }
      
-     matrix<-x$get()         # no cached matrix so get the matrix
+     matrix<-x$get()         # no cached matrix so get the matrix object
      m <- solve(matrix, ...) # calculate the inverted matrix
      x$setmatrix(m)          # store the inverted matrix object created in makeCacheMatrix
      m                       # return the inverted matrix
